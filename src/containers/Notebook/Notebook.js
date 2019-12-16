dee4ff766de3c2aa5c0315745ae9fe25318fc573
Joshua Shouppe <josh@Joshuas-MBP-2.home>
2019-12-16 13:48:24 -0500

    notebook group

--- src/containers/Notebook/Notebook.js
+++ src/containers/Notebook/Notebook.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Notebook.styles.css';
 import NotebookSearch from '../NotebookSearch/NotebookSearch';
 import NotebookList from '../NotebookList/NotebookList';
+import NotebookButtonGroup from '../NotebookButtonGroup/NotebookButtonGroup';
 
 function Notebook() {
   return (
@@ -9,6 +10,7 @@ function Notebook() {
       <h2 style={{ margin: 0, padding: 10 }}>Notebook Title</h2>
       <NotebookSearch />
       <NotebookList />
+      <NotebookButtonGroup />
     </div>
   );
 }
--- src/containers/NotebookButtonGroup/NotebookButtonGroup.js
+++ src/containers/NotebookButtonGroup/NotebookButtonGroup.js
@@ -1,5 +1,30 @@
-{/* <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
-  <Button>One</Button>
-  <Button>Two</Button>
-  <Button>Three</Button>
-</ButtonGroup> */}
+import React from 'react';
+import './NotebookButtonGroup.styles.css';
+
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+
+// Icons
+import SystemUpdateAltIcon from '@material-ui/icons/SystemUpdateAlt';
+import PublishIcon from '@material-ui/icons/Publish';
+
+function NotebookButtonGroup() {
+  return (
+    <div className='NotebookButtonGroup'>
+      <ButtonGroup
+        color="primary"
+        aria-label="contained primary button group"
+        size="small"
+      >
+        <Button variant="contained" startIcon={<SystemUpdateAltIcon />}>
+          Import
+        </Button>
+        <Button startIcon={<PublishIcon />}>
+          Export
+        </Button>
+      </ButtonGroup>
+    </div>
+  );
+}
+
+export default NotebookButtonGroup;
