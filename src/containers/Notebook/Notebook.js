da3211e60f1717639a7cd76f86c05c3948919394
Joshua Shouppe <josh@Joshuas-MBP-2.home>
2019-12-16 12:31:59 -0500

    layout

---
+++ src/containers/Notebook/Notebook.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Notebook.styles.css';
+
+function Notebook() {
+  return (
+    <div className='Notebook'>
+      Notebook
+    </div>
+  );
+}
+
+export default Notebook;
---
+++ src/containers/PaintingArea/PaintingArea.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PaintingArea.styles.css';
+
+function PaintingArea() {
+  return (
+    <div>
+      PaintingArea
+    </div>
+  );
+}
+
+export default PaintingArea;
